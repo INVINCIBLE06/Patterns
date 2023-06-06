6c7aab8d76c850527a0b0c922c82157c8f2c1502
[Saurabh Pande] <[sp832154@gmail.com]>
2023-06-06 16:49:46 +0530

    Pattern 24

---
+++ Pattern/Pattern 24/for.cpp
@@ -0,0 +1,33 @@
+/**
+ * The below code is for printing the partter
+ *          The pattern will be like this :-
+                  1 2 3 4
+                    2 3 4
+                      3 4
+                        4
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n = 4;
+    for(int i = 1; i <= n; i++)
+    {
+        for(int j = 1; j <= (n - i); j++)
+        {
+            cout<<"    ";
+        }
+
+        for (int k = 1; k <= i; k++)
+        {
+            cout<< k;
+            if (k != i) 
+            {
+                cout<<"   ";
+            }
+        }
+        cout<<endl;
+    }
+}
